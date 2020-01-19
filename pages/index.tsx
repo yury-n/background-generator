252fff95b898c6853ec6c5cad5e25e4cc8f5dfe5
Alex Lebedev <generatiff.com@gmail.com>
2020-01-19 23:51:43 +0400

    version with antd

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,11 @@
 import { NextPage } from "next";
+import { DatePicker } from "antd";
 
 const Home: NextPage<{ userAgent?: string }> = ({ userAgent }) => (
-  <h1>Hello world! - user agent: {userAgent}</h1>
+  <>
+    <h1>Hello world! - user agent: {userAgent}</h1>
+    <DatePicker />
+  </>
 );
 
 Home.getInitialProps = async ({ req }) => {
