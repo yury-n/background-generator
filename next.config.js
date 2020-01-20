334de16bf682200285ec2ba30d56c62623578b95
Alex Lebedev <generatiff.com@gmail.com>
2020-01-20 12:32:37 +0400

    with ant design and typescript

--- next.config.js
+++ next.config.js
@@ -1,28 +1,40 @@
 /* eslint-disable */
-const withCss = require("@zeit/next-css");
+const withLess = require('@zeit/next-less')
+const lessToJS = require('less-vars-to-js')
+const fs = require('fs')
+const path = require('path')
 
-module.exports = withCss({
+// Where your antd-custom.less file lives
+const themeVariables = lessToJS(
+  fs.readFileSync(path.resolve(__dirname, './assets/antd-custom.less'), 'utf8')
+)
+
+module.exports = withLess({
+  lessLoaderOptions: {
+    javascriptEnabled: true,
+    modifyVars: themeVariables, // make your antd custom effective
+  },
   webpack: (config, { isServer }) => {
     if (isServer) {
-      const antStyles = /antd\/.*?\/style\/css.*?/;
-      const origExternals = [...config.externals];
+      const antStyles = /antd\/.*?\/style.*?/
+      const origExternals = [...config.externals]
       config.externals = [
         (context, request, callback) => {
-          if (request.match(antStyles)) return callback();
-          if (typeof origExternals[0] === "function") {
-            origExternals[0](context, request, callback);
+          if (request.match(antStyles)) return callback()
+          if (typeof origExternals[0] === 'function') {
+            origExternals[0](context, request, callback)
           } else {
-            callback();
+            callback()
           }
         },
-        ...(typeof origExternals[0] === "function" ? [] : origExternals)
-      ];
+        ...(typeof origExternals[0] === 'function' ? [] : origExternals),
+      ]
 
       config.module.rules.unshift({
         test: antStyles,
-        use: "null-loader"
-      });
+        use: 'null-loader',
+      })
     }
-    return config;
-  }
-});
+    return config
+  },
+})
