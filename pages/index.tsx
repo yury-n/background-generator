81f7247c1e2ae22a182a4b5d85f9aa40a24adca4
Alex Lebedev <generatiff.com@gmail.com>
2020-02-16 01:02:03 +0400

    add liveinternet counter

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,6 +11,26 @@ import ConfigPanel from "../components/ConfigPanel";
 import "antd/dist/antd.css";
 import s from "./index.less";
 
+const LICounter = () => (
+  <div
+    className={s["stat-counter"]}
+    dangerouslySetInnerHTML={{
+      __html: `<!--LiveInternet counter--><script type="text/javascript">
+document.write('<a href="//www.liveinternet.ru/click" '+
+'target="_blank"><img src="//counter.yadro.ru/hit?t26.6;r'+
+escape(document.referrer)+((typeof(screen)=='undefined')?'':
+';s'+screen.width+'*'+screen.height+'*'+(screen.colorDepth?
+screen.colorDepth:screen.pixelDepth))+';u'+escape(document.URL)+
+';h'+escape(document.title.substring(0,150))+';'+Math.random()+
+'" alt="" title="LiveInternet: показано число посетителей за'+
+' сегодня" '+
+'border="0" width="88" height="15"><\/a>')
+</script><!--/LiveInternet-->
+`
+    }}
+  />
+);
+
 const IndexPage = () => {
   return (
     <>
@@ -45,6 +65,7 @@ const IndexPage = () => {
         </Layout>
       </Layout>
       <div className={s["extra-layout"]}></div>
+      <LICounter />
     </>
   );
 };
